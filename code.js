3005c1ce2b068cefa305162e0d8f3cd4821130bf
Brittney Brinson <brittneyc95@outlook.com>
2022-09-15 17:51:31 -0400

    Starting question 10 with Jasmine

--- code.js
+++ code.js
@@ -3,6 +3,8 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello (x){
+    if (x === true || x === false)
+        return "Hello, World!"
     return "Hello, " + x + "!"
 }
 
